f3b05bb0378423d055cd5566cb1cd87e5dd41a9a
luis arthur <luisarthurlards03@gmail.com>
2026-04-21 13:43:25 -0300

    fix: :recycle: diminuindo a mistura dos videos editados com os não editados

--- scripts/edit_video.py
+++ scripts/edit_video.py
@@ -1,5 +1,7 @@
 from moviepy import VideoFileClip
+import os
 def cut_video(input_file : str, start_time : float, end_time : float):
-    output_file = f"{input_file.replace('.mp4', '')}_cut_{start_time}_{end_time}.mp4"
+    filename = os.path.basename(input_file)
+    output_file = f"../videos cortados/{filename.replace('.mp4', '')}_cut_{start_time}_{end_time}.mp4"
     c1 = VideoFileClip(input_file)
     c1.subclipped(start_time, end_time).write_videofile(output_file)
