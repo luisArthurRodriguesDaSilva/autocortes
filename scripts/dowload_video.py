6e7834759313e02ca6ab693353ac85ef75ad90f2
luis arthur <luisarthurlards03@gmail.com>
2026-04-21 22:29:14 -0300

    fix: :construction: separando pelo nome videos em alta e baixa qualidade

--- scripts/dowload_video.py
+++ scripts/dowload_video.py
@@ -1,15 +1,27 @@
 import yt_dlp
+import os
 
+def verify_file_exist(file_path: str) -> bool:
+    return os.path.isfile(file_path)
+    
 
 def download_video_high_resolution(video_id: str):
-    ydl_opts = {"outtmpl": f"../videos inteiros/h_{video_id}.mp4"}
+    output_file = f"../videos inteiros/h_{video_id}.mp4"
+    if verify_file_exist(output_file):
+        print(f"Arquivo {output_file} já existe. Pulando download.")
+        return 0
+    ydl_opts = {"outtmpl": output_file, "format": "bestvideo+bestaudio/best"}
     
     url = f"https://www.youtube.com/watch?v={video_id}"
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
 
 def download_video_smalest_resolution(video_id: str):
-    ydl_opts = {"outtmpl": f"../videos inteiros/l_{video_id}.mp4", "format": "worst"}
+    output_file = f"../videos inteiros/l_{video_id}.mp4"
+    if verify_file_exist(output_file):
+        print(f"Arquivo {output_file} já existe. Pulando download.")
+        return 0
+    ydl_opts = {"outtmpl": output_file, "format": "worst"}
     
     url = f"https://www.youtube.com/watch?v={video_id}"
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
